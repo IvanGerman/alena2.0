227b84f24c47ae56e0a6718d8a3c6f5b408bbed8
IvanGerman <68238341+IvanGerman@users.noreply.github.com>
2023-04-01 20:01:33 +0700

    Update contactForm.js

--- src/modules/contactForm.js
+++ src/modules/contactForm.js
@@ -35,22 +35,3 @@ async function sendEmail() {
 
 
 
-// Email.send({
-//   Host : "smtp.elasticemail.com",
-//   Username : "username",
-//   Password : "3CF6C68C3B8109FD9753FFC2DD8C83686994",
-//   To : 'ivgvi@mail.ru',
-//   From : "ivgvi@mail.ru",
-//   Subject : "This is the subject",
-//   Body : "And this is the body"
-// }).then(
-// message => alert(message)
-// );
-
-// token 963fe688-fe71-448d-87b0-f18bb220d1b5
-// pw 3CF6C68C3B8109FD9753FFC2DD8C83686994
-
-
-// for newbravestallion@gmail.com
-// pw CC27E2D0DB676A9830C1706A50135628DD31
-// token ff10c9f0-f510-4582-8a16-30a19c6f0a4d
